91dd8ede7188a45095415b9fc7a665f5b119aa01
FilippoBid <filippoperrone1997@gmail.com>
2022-11-09 11:40:17 +0100

    finisco i 2 esercizi

--- JS/main.js
+++ JS/main.js
@@ -33,12 +33,16 @@ function wordDisassemble(word) {
  */
 function palindrom(wordArray) {
     if (wordArray instanceof Array) {
+        let pali = 0;
 
         for (i = 0; i <= (wordArray.length / 2); i++) {
-            if (wordArray[i] === wordArray[wordArray.length - (i + 1)]) {
-                return console.log("la parola é palindroma");
-            } else {return console.log("la parona non é palindorma");}
+            if (wordArray[i] !== wordArray[wordArray.length - (i + 1)]) {
+                pali++;
+            }
         }
+        if (pali > 0) { console.log("la parola non é palindroma"); }
+        else { console.log("la parola é palindroma"); }
+
 
     } else {
         return console.log("inserire un array come input");
@@ -52,11 +56,52 @@ function palindrom(wordArray) {
 
 }
 
+function paridisp(userNumber, oddOrNot) {
+    aiNumber = Math.floor(Math.random() * 6);
+    userNumber = parseInt(userNumber);
+
+    if (typeof userNumber !== "number") {
+        return console.log("inserire un numero");
+    }
+    if (userNumber > 5) {
+        return console.log("inserire un numero da 1 a 5");
+    }
+    if (oddOrNot === "pari") {
+        if ((aiNumber + userNumber) % 2 === 0) {
+            return console.log(`il numero del Ai é ${aiNumber}, hai vinto!`);
+        }
+        else if ((aiNumber + userNumber) % 2 !== 0) {
+            return console.log(`il numero del Ai é ${aiNumber}, hai perso!`);
+        }
+    }
+    if (oddOrNot === "dispari") {
+        if ((aiNumber + userNumber) % 2 === 0) {
+            return console.log(`il numero del Ai é ${aiNumber}, hai perso!`);
+        }
+        else if ((aiNumber + userNumber) % 2 !== 0) {
+            return console.log(`il numero del Ai é ${aiNumber}, hai vinto!`);
+        }
+    }
+    /* aiNumber = Math.floor(Math.random() * 6);
+    if (userNumber > 5 && typeof userNumber !== "number") { return console.log("inserire un numero da 1 a 5"); };
+    if ((aiNumber + userNumber) % 2 === 0 && oddOrNot === "pari"){
+        return console.log(`il numero del Ai é ${aiNumber}, hai vinto!`);
+
+    } else if((aiNumber + userNumber) % 2 === 0 && oddOrNot === "dispari"){
+        return console.log(`il numero del Ai é ${aiNumber}, hai perso!`);
+    }  */
+}
+
+
+
+
 
 btnStart.addEventListener("click", function () {
 
 
     palindrom(wordDisassemble(wordInEl.value));
+
+    paridisp(numberInEl.value, pariDispariEl.value);
 
 
 
